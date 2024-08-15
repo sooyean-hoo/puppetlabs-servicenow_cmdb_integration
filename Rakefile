a7bf8027ae372ee4fb294f76e944e7a84c46d66c
Hoo Sooyean (何書淵) <Sooyean.hoo@perforce.com>
2024-08-15 15:58:20 +0800

    Exposing more Rake tasks and comforting to the Litmus standards

--- Rakefile
+++ Rakefile
@@ -7,6 +7,8 @@ require 'puppet_blacksmith/rake_tasks' if Bundler.rubygems.find_name('puppet-bla
 require 'github_changelog_generator/task' if Bundler.rubygems.find_name('github_changelog_generator').any?
 require 'puppet-strings/tasks' if Bundler.rubygems.find_name('puppet-strings').any?
 
+require_relative './spec/support/acceptance/vhelper'
+
 def changelog_user
   return unless Rake.application.top_level_tasks.include? "changelog"
   returnVal = nil || JSON.load(File.read('metadata.json'))['author']
--- spec/support/acceptance/vhelper.rb
+++ spec/support/acceptance/vhelper.rb
@@ -314,8 +314,95 @@ exit
 # lint:endignore
 # rubocop:enable all
 # puts 'running as ruby'
-require './helpers.rb'
 
-module VHelpers
-  extend TargetHelpers
+## Implemented own versions: require_relative './helpers.rb'
+
+module VP
+  require 'puppet_litmus'
+  PuppetLitmus.configure!
+  
+  class Target
+    include PuppetLitmus
+  
+    attr_reader :uri
+  
+    def initialize(uri)
+      @uri = uri
+    end
+  
+    def bolt_config
+      inventory_hash = LitmusHelpers.inventory_hash_from_inventory_file
+      LitmusHelpers.config_from_node(inventory_hash, @uri)
+    end
+  
+    # Make sure that ENV['TARGET_HOST'] is set to uri
+    # before each PuppetLitmus method call. This makes it
+    # so if we have an array of targets, say 'agents', then
+    # code like agents.each { |agent| agent.bolt_upload_file(...) }
+    # will work as expected. Otherwise if we do this in, say, the
+    # constructor, then the code will only work for the agent that
+    # most recently set the TARGET_HOST variable.
+    PuppetLitmus.instance_methods.each do |name|
+      m = PuppetLitmus.instance_method(name)
+      define_method(name) do |*args, &block|
+        ENV['TARGET_HOST'] = uri
+        m.bind(self).call(*args, &block)
+      end
+    end
+  end
+  
+  class TargetNotFoundError < StandardError; end
+  module TargetHelpers
+    def master
+      target('master', 'acceptance:provision_vms', 'master')
+    end
+    module_function :master
+  
+    def servicenow_instance
+      target('ServiceNow instance', 'acceptance:setup_servicenow_instance', 'servicenow_instance')
+    end
+    module_function :servicenow_instance
+  
+    def target(name, setup_task, role)
+      @targets ||= {}
+  
+      unless @targets[name]
+        # Find the target
+        inventory_hash = LitmusHelpers.inventory_hash_from_inventory_file
+        targets = LitmusHelpers.find_targets(inventory_hash, nil)
+        target_uri = targets.find do |target|
+          vars = LitmusHelpers.vars_from_node(inventory_hash, target) || {}
+          roles = vars['roles'] || []
+          roles.include?(role)
+        end
+        unless target_uri
+          raise TargetNotFoundError, "none of the targets in 'inventory.yaml' have the '#{role}' role set. Did you forget to run 'rake #{setup_task}'?"
+        end
+        @targets[name] = Target.new(target_uri)
+      end
+  
+      @targets[name]
+    end
+    module_function :target
+  end
+  
+  module LitmusHelpers
+    extend PuppetLitmus
+  end
 end
+
+
+namespace :valentepuppet do
+  require 'puppet_litmus/rake_tasks'
+  #require_relative './helpers'
+
+  include VP::TargetHelpers
+  
+  desc 'Testing of Parameters'
+  task :parametertests, [:para1, :para2] do |t, paras|
+   master = VP::Target.new('AAAAAA')
+    a = master.uri
+    puts "Hello...#{paras[:para1]}...#{paras[:para2]}...#{a}"
+  end
+end    
+
