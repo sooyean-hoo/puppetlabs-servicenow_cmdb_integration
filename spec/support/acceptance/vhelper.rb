fb66dea76b464433996a845e1326fdabda72f95b
Hoo Sooyean (何書淵) <Sooyean.hoo@perforce.com>
2024-08-17 18:51:47 +0800

    Better clean up and  handler or error code

--- spec/support/acceptance/vhelper.rb
+++ spec/support/acceptance/vhelper.rb
@@ -314,15 +314,15 @@ function      command(){
         (sleep 1800 && ssh  -i /tmp/myownkey -A -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oTCPKeepAlive=yes -oServerAliveInterval=10  -p${deploype_port} -l vagrant ${deploype_ip}  "rm -fr  /tmp/proxy.txt"  ) &
         echoMsg '==' "Setup done, Now Run Tests"  ;
         bundle exec "rake acceptance:run_tests" ; errorid=$? ;
-        #### Hardcoded for now 
-        ssh  -i /tmp/myownkey -A -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oTCPKeepAlive=yes -oServerAliveInterval=10   -p${deploype_port} -l vagrant ${deploype_ip}  "rm -fr  /tmp/proxy.txt"  ;
-        #### 
-
+        echo "rake acceptance:run_tests done with errid=$errorid " ;
         exit $errorid ;
 }
 
 function      postcommand(){
         
+        #### Hardcoded for now 
+        ssh  -i /tmp/myownkey -A -oStrictHostKeyChecking=no -oUserKnownHostsFile=/dev/null -oTCPKeepAlive=yes -oServerAliveInterval=10   -p${deploype_port} -l vagrant ${deploype_ip}  "rm -fr  /tmp/proxy.txt"  ;
+        #### 
         bundle exec "rake acceptance:tear_down"  || echo  "Tear Down also have errors." ;
         
         errorid=$? ;
