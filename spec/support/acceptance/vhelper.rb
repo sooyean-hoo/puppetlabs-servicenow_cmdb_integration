1bab020db5ccd235e2e6990206573b3598306b70
Hoo Sooyean (何書淵) <Sooyean.hoo@perforce.com>
2024-08-26 04:53:49 +0800

    Reverse port serving for service now ... on testing of non-ubuntu machines

--- spec/support/acceptance/vhelper.rb
+++ spec/support/acceptance/vhelper.rb
@@ -362,7 +362,7 @@ function      command(){
         if [[  $platforms_image =~ buntu ]]    ; then
           portsfwdOptions="-L:8140:127.0.0.1:8140 -L:8143:127.0.0.1:8143 -L:1080:127.0.0.1:1080" ;
         else
-          portsfwdOptions="-L:8140:127.0.0.1:8140 -L:8143:127.0.0.1:8143 " ;
+          portsfwdOptions="-L:8140:127.0.0.1:8140 -L:8143:127.0.0.1:8143 -R:1080:127.0.0.1:1080" ;
         fi ;
         masterip=${deploype_ip} ;
         echoMsg '++'    ;
