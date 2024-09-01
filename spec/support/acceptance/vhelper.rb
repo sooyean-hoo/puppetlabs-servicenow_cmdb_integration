f6b4617860136dd20fbf15227d79cb92a162e2a1
Hoo Sooyean (何書淵) <Sooyean.hoo@perforce.com>
2024-09-02 04:41:08 +0800

    Ensure RW of /etc/puppetlabs/puppet and files

--- spec/support/acceptance/vhelper.rb
+++ spec/support/acceptance/vhelper.rb
@@ -712,6 +712,10 @@ function      command(){ # Filed under acceptance
         source /tmp/v.sh  loadlib  ;
         echoMsg '!!' "Running the actual Acceptance Tests" || echo "============================Running the actual Acceptance Tests============================== " ;
         
+        mkdir -p /etc/puppetlabs/puppet ;
+        sudo chmod a+rw /etc/puppetlabs/puppet/servicenow_cmdb.yaml || chmod a+rw /etc/puppetlabs/puppet/servicenow_cmdb.yaml || true ;
+        sudo chmod -R a+rw /etc/puppetlabs/puppet || chmod -R a+rw/etc/puppetlabs/puppet || true ;
+  
         echoMsg '==' Preparing PE Server 
         bundle update ;
         bundle install ;
